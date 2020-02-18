a98fb60e3a10deccdf2e78246e66a2eef503ffd8
Thilina Rathnayake <thilinarmtb@gmail.com>
2020-02-18 02:04:15 +0000

    [GPU] WIP: Fortran interface

---
+++ core/accel.f
@@ -0,0 +1,10 @@
+      subroutine setup_accel(accel_h)
+      include 'SIZE'
+
+      integer accel_h
+      common /nekmpi/ mid,mp,nekcomm,nekgroup,nekreal
+
+      include 'exaf.h'
+
+      return
+      end
