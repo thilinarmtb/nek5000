2b9974237ad92038f6037ea3c581b959207c1b05
Thilina Rathnayake <thilinarmtb@gmail.com>
2020-09-27 00:07:34 -0400

    Use parRSB metrics

--- core/partitioner.c
+++ core/partitioner.c
@@ -321,7 +321,9 @@ void fpartMesh(long long *el, long long *vl, double *xyz,
 
   part = (int*) malloc(*lelt * sizeof(int));
 
-  /* printPartStat(vl, nel, nv, cext); */
+  if(comm.id==0)
+    printf("Before partitioning:\n");
+  printPartStat(vl, nel, nv, cext);
 
   ierr = 1;
 #if defined(PARRSB)
@@ -340,7 +342,9 @@ void fpartMesh(long long *el, long long *vl, double *xyz,
     ierr=redistributeData(&nel,vl,el,part,nv,*lelt,&comm);
     if (ierr != 0) goto err;
 
-    /* printPartStat(vl, nel, nv, cext); */
+    if(comm.id==0)
+      printf("After RCB:\n");
+    printPartStat(vl, nel, nv, cext);
   }
 
   if(rsb){
@@ -352,6 +356,11 @@ void fpartMesh(long long *el, long long *vl, double *xyz,
 
     /* printPartStat(vl, nel, nv, cext); */
   }
+
+  if(comm.id==0)
+    printf("After partitioning:\n");
+  printPartStat(vl, nel, nv, cext);
+
 #elif defined(PARMETIS)
   int metis; metis=mode&4;
 
